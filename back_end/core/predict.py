447d7c45afb03d183cb6833d6e8d9c0cc040d7d9
高鹏 <56418935+trounique@users.noreply.github.com>
2022-01-18 20:35:13 +0800

    后端

---
+++ back_end/core/predict.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+
+def predict(dataset, model, ext):
+    global img_y
+    # x = dataset[0].replace('\\', '/')
+    x = dataset[0]
+    file_name = dataset[1]
+    print(x)
+    print(file_name)
+    # x = cv2.imread(x)
+    x = Image.open(x).convert('RGB')
+    x = np.array(x)
+    img_y, image_info = model.detect(x)
+    cv2.imwrite('./tmp/draw/{}.{}'.format(file_name, ext), img_y)
+    # raise Exception('保存图片时出错.Error saving thepicture.')
+    return image_info

b2fbe95aeee48da35f8f9fcf2662fe9a8950ce1e
高鹏 <56418935+trounique@users.noreply.github.com>
2022-01-18 20:59:01 +0800

    后端

--- yolo_flask_vue/app.py
+++ yolo_flask_vue/app.py
@@ -62,7 +62,7 @@ def upload_file():
 
 @app.route("/download", methods=['GET'])
 def download_file():
-    # 需要知道2个参数, 第1个参数是本地目录的path, 第2个参数是文件名(带扩展名).
+    # 需要知道2个参数, 第1个参数是本地目录的path, 第2个参数是文件名(带扩展名)..
     return send_from_directory('data', 'testfile.zip', as_attachment=True)
 
 

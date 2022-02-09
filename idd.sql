92f33c73c5c2fdcec0266c6608cf190647b22f63
高鹏 <56418935+trounique@users.noreply.github.com>
2022-02-09 22:06:37 +0800

    2/9Update：Mysql表结构增加细节

--- idd.sql
+++ idd.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 03/02/2022 15:31:12
+ Date: 09/02/2022 22:01:26
 */
 
 SET NAMES utf8mb4;
@@ -25,21 +25,24 @@ CREATE TABLE `results`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime(0) NULL DEFAULT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `mainboard_good` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `interface_good` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `mainboard_lack` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `interface_lack` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `fan_good` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `fan_lack` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of results
 -- ----------------------------
-INSERT INTO `results` VALUES (1, '2022-01-27 18:01:50', 'origin11.jpg');
-INSERT INTO `results` VALUES (2, '2022-01-27 19:01:46', 'origin11.jpg');
-INSERT INTO `results` VALUES (3, '2022-01-27 19:01:34', 'origin11.jpg');
-INSERT INTO `results` VALUES (4, '2022-01-27 20:01:52', 'origin11.jpg');
-INSERT INTO `results` VALUES (5, '2022-01-27 21:01:11', 'origin11.jpg');
-INSERT INTO `results` VALUES (6, '2022-01-28 00:01:02', 'origin11.jpg');
-INSERT INTO `results` VALUES (7, '2022-01-28 00:01:16', 'origin11.jpg');
-INSERT INTO `results` VALUES (8, '2022-01-28 00:01:05', 'origin12.jpg');
-INSERT INTO `results` VALUES (9, '2022-01-28 00:01:38', 'origin11.jpg');
-INSERT INTO `results` VALUES (10, '2022-01-28 00:01:53', 'origin11.jpg');
+INSERT INTO `results` VALUES (1, '2022-02-09 19:02:12', 'origin2 (104).jpg', '6', '1', '2', '0', '3', '1');
+INSERT INTO `results` VALUES (2, '2022-02-09 20:02:01', 'origin2 (104).jpg', '6', '1', '2', '0', '3', '1');
+INSERT INTO `results` VALUES (3, '2022-02-09 20:02:53', 'origin2 (104).jpg', '6', '1', '2', '0', '3', '1');
+INSERT INTO `results` VALUES (4, '2022-02-09 20:02:20', 'origin2 (104).jpg', '6', '1', '2', '0', '3', '1');
+INSERT INTO `results` VALUES (5, '2022-02-09 21:02:18', 'origin2 (165).jpg', '3', '1', '3', '0', '2', '2');
+INSERT INTO `results` VALUES (6, '2022-02-09 21:02:48', 'origin2 (112).jpg', '6', '1', '2', '0', '0', '4');
+INSERT INTO `results` VALUES (7, '2022-02-09 22:02:15', 'origin2 (173).jpg', '3', '1', '2', '0', '1', '3');
 
 SET FOREIGN_KEY_CHECKS = 1;
